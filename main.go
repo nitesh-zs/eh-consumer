546e6786ccfed248827c405465ad95e6dedb4436
Nitesh Saxena <nitesh.saxena@zopsmart.com>
2022-09-13 19:45:32 +0530

    update schema

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/krogertechnology/data-loader/pkg/loader"
+	"github.com/krogertechnology/data-loader/pkg/models"
 	"github.com/krogertechnology/krogo/pkg/krogo"
 )
 
@@ -22,6 +24,8 @@ func load(c *krogo.Context) (i interface{}, err error) {
 		return "Failed", err
 	}
 
+	_, _ = l.AddConfigLayerWithNeeds(write, 1)
+
 	// start loader
 	if err := l.Start(c); err != nil {
 		return "Failed", err
@@ -30,4 +34,21 @@ func load(c *krogo.Context) (i interface{}, err error) {
 	c.Logger.Info("Data Loader Completed Successfully")
 
 	return "Done!", nil
+}
+
+func write(c *krogo.Context, record models.Record) (rec *models.Record, err error) {
+	rawData := record.Value["raw_payload"].(string)
+
+	data := make(map[string]interface{})
+
+	err = json.Unmarshal([]byte(rawData), &data)
+	if err != nil {
+		c.Logger.Error(err)
+		return nil, nil
+	}
+
+	record.Value["data"] = data["data"]
+	record.Value["eventHeader"] = data["eventHeader"]
+
+	return &record, nil
 }
